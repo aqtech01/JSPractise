014f82f24605f72ab662648576ede7df201d5782
AQTECH <abdulqadeer0120@gmail.com>
2024-03-17 21:55:24 +0000

    end of the First Day

--- 01.Basics/index.js
+++ 01.Basics/index.js
@@ -1,7 +1,150 @@
 // alert("Hello! THis is Sparta!")
 console.log("This is Sparta!");
-console.log("This is Sparta!");
-console.log("This is Sparta!");
-console.log("This is Sparta!");
-console.log("This is Sparta!");
+// Variable => a box to store a data 
+// reserved keyword (variable name) (type of value)
+// var => can change any time
+// let => cahnge values any but not declared again 
 
+let name = "Abdul Qadeer";
+console.log(name);
+// only we change values but not declare again 
+name = "AQ";
+console.log(name);
+
+// declare a variable and give value other time 
+
+let age ;
+age = 20;
+console.log(age);
+
+// declare one time and assign value one time 
+const gender = "Male";
+// abc = "This is Sparta";
+console.log(gender);
+let twitterHandle = "@abdulqadeer0011";
+console.log(twitterHandle);
+
+
+// Data Types 
+// Primitve type 
+// 1- Number
+let num = 4
+let num1=12;
+let num2=3;
+console.log(num+num1);
+console.log(typeof num);
+console.log(typeof num2);
+console.log(typeof(name));
+
+
+// Basic Math 
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+console.log(num1**num2);
+
+let counter = 0;
+console.log(counter);
+counter++
+console.log(counter);
+counter--
+console.log(counter);
+
+
+
+// Boolean Operators
+
+let isTrue = true;
+console.log(isTrue);
+let numer = NaN;
+console.log(numer);
+
+
+// falsy value
+// false 
+// null
+// undefined
+// 0 
+// -0
+// NaN
+// '',"" empty quotes
+
+
+let notDefined = undefined;
+console.log(notDefined);
+let notnull = null;
+console.log(notnull);
+
+
+
+// Comparison Operators
+
+// Relational Operators
+// <,>,<=,>=,
+
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 >= 20);
+console.log(10 <= 20);
+
+
+// <--------------------->
+
+// Equality operators 
+// Strict operators  
+// ===,!==
+// loss operators
+
+console.log(10 == "10");
+console.log(10 != "10");
+console.log(10 === "10");
+console.log(10 !== "10");
+
+
+// <----------------------->
+
+// Relational 
+
+let firstFavNumber= 12;
+let secFavNum = 13;
+console.log(firstFavNumber > secFavNum);
+console.log(firstFavNumber < secFavNum);
+console.log(firstFavNumber >= secFavNum);
+console.log(firstFavNumber >= secFavNum);
+
+
+// <---------------------------->
+
+// Equality
+
+
+
+console.log(firstFavNumber == secFavNum);
+console.log(firstFavNumber === secFavNum);
+console.log(firstFavNumber !== secFavNum);
+console.log(firstFavNumber != secFavNum);
+
+
+
+// Strings
+
+let firstName = "Abdul ";
+let lastName = "Qadeer";
+// console.log(firstName +" "+ lastName);
+
+// concatination
+
+// let fullName = firstName .concat(lastName);
+// console.log(fullName);
+
+
+// Append 
+// firstName += lastName;
+// console.log(firstName);
+
+
+// length 
+console.log(firstName.length);
