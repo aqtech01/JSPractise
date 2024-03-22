b48ec698aee9f336e9dc2c73c58c6cd4cb63ae1f
AQTECH <abdulqadeer0120@gmail.com>
2024-03-22 12:08:47 +0000

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -1,13 +1,1 @@
-import mysql.connector
- 
-dataBase = mysql.connector.connect(
-  host ="localhost",
-  user ="root",
-  passwd ="Khawaja362001@"
-)
- 
-# preparing a cursor object
-cursorObject = dataBase.cursor()
- 
-# creating database
-cursorObject.execute("CREATE DATABASE resume_app")
+
